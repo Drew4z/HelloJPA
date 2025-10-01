d665f6b92f672e7f8957c02266d2b4fd3d4a6591
Drew4z <andrewwac27@gmail.com>
2025-10-01 10:48:49 +0200

    First commit

---
+++ src/main/java/com/HelloJPA/HelloJPA/domain/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.HelloJPA.HelloJPA.domain.repository;
+import java.util.List;
+
+import com.HelloJPA.HelloJPA.domain.entities.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
